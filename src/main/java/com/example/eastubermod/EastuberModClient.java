e72c1b101c74e48aed4b0290a59eba9ff49dc6e8
Mantraism <adinugrahabaskara@gmail.com>
2026-01-27 22:45:04 +0700

    Initial commit

---
+++ src/main/java/com/example/eastubermod/EastuberModClient.java
@@ -0,0 +1,10 @@
+package com.example.eastubermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EastuberModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/example/eastubermod/EastuberModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.example.eastubermod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EastuberModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
